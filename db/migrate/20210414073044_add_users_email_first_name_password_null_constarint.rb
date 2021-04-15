20ee2207ac49ab21a2888251ee9d522f8b968984
Fanis <habfanis@mail.ru>
2021-04-15 11:08:40 +0300

    Fix task_2

--- db/migrate/20210414073044_add_users_email_first_name_password_null_constarint.rb
+++
@@ -1,7 +0,0 @@
-class AddUsersEmailFirstNamePasswordNullConstarint < ActiveRecord::Migration[6.1]
-  def change
-    change_column_null(:users, :email, false)
-    change_column_null(:users, :first_name, false)
-    change_column_null(:users, :password, false)
-  end
-end
