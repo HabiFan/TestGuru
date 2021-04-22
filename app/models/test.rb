7b676c6ce3db8792d6dc48461b6513c5767841b9
Fanis <habfanis@mail.ru>
2021-04-22 11:54:47 +0300

    Fix task_3

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,7 +3,7 @@ class Test < ApplicationRecord
     def tests_by_categories(title)
       joins('INNER JOIN categories ON tests.category_id = categories.id')
       .where(categories: { title: title })
-      .order(title: :desc)
+      .order(title: :desc).pluck(:title)
     end
   end
 end
