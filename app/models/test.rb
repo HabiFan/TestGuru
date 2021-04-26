45faf860cd0e3b81f292ce152e66ed1565eeddcc
Fanis <habfanis@mail.ru>
2021-04-26 10:53:47 +0300

    Add task_4

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :author, class_name: 'User'
+
+  has_many :passing_tests, dependent: :destroy
+  has_many :users, through: :passing_tests
+
   class << self
     def tests_by_categories(title)
       joins('INNER JOIN categories ON tests.category_id = categories.id')
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
 
+  has_many :passing_tests, dependent: :destroy
+  has_many :tests, through: :passing_tests
+  has_many :authors, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
+
   def level_by_tests(level)
     Test.joins('INNER JOIN passing_tests ON tests.id = passing_tests.test_id')
         .where(passing_tests: { user_id: id })
