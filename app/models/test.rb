e4d81bc71db5125ccd91197af25b6189466bac35
Fanis <habfanis@mail.ru>
2021-05-04 10:30:40 +0300

    Task_5

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,11 +5,20 @@ class Test < ApplicationRecord
   has_many :passing_tests, dependent: :destroy
   has_many :users, through: :passing_tests
 
+  validates :title, presence: true, uniqueness: true
+  validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  scope :level, ->(level) { where(level: level) }
+  scope :simple, -> { level(0..1) }
+  scope :medium, -> { level(2..4) }
+  scope :difficult , -> { level(5..Float::INFINITY) }
+  scope :by_category, ->(title) { joins(:category)
+                                  .where(categories: { title: title })
+                                  .order(title: :desc) }
+
   class << self
     def tests_by_categories(title)
-      joins(:category)
-      .where(categories: { title: title })
-      .order(title: :desc).pluck(:title)
+      by_category(title).pluck(:title)
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,7 @@ class User < ApplicationRecord
   has_many :authored_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
 
   def level_by_tests(level)
-    Test.joins(:passing_tests)
-        .where(passing_tests: { user_id: id })
-        .where(level: level)
+    passing_tests.by_level(level)
   end
 
 end
