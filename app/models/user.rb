2343b6a101c6f646f71d2733b0dadb28d816a348
Fanis <habfanis@mail.ru>
2021-05-05 10:29:40 +0300

    Fix task_5 user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :tests, through: :passing_tests
   has_many :authored_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
 
+  validates :first_name, :password, :email, presence: true
+
   def level_by_tests(level)
     passing_tests.by_level(level)
   end
