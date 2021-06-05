c152e209bdf61df9aad0b824ab2f0f3491e8a6e8
Fanis <habfanis@mail.ru>
2021-06-06 02:31:10 +0300

    Fix task_8

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,2 +1,7 @@
 module TestPassagesHelper
+
+  def show_number_question(current_question)
+    "Question (#{@test_passage.current_question_number(current_question)} /
+    #{@test_passage.test.questions.count})"
+  end
 end
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -4,6 +4,15 @@ class TestPassage < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_next_question, on: :update
+
+  def current_question_number(current_question)
+    test.questions.order(:id).pluck(:id).index(current_question.id) + 1
+  end
+
+  def total_percent_correct_answers
+    (correct_questions * 100) / test.questions.count
+  end
 
   def completed?
     current_question.nil?
@@ -11,10 +20,9 @@ class TestPassage < ApplicationRecord
 
   def accept!(answer_ids)
     if correct_answer?(answer_ids)
-      self.correct_question += 1
+      self.correct_questions += 1
     end
 
-    self.current_question = next_question
     save!
   end
 
@@ -25,10 +33,7 @@ class TestPassage < ApplicationRecord
   end
 
   def correct_answer?(answer_ids)
-    correct_answers_count = correct_answers.count
-
-    correct_answers_count == correct_answers.where(id: answer_ids).count &&
-    correct_answers_count == answer_ids.count
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
   end
 
   def correct_answers
@@ -39,4 +44,7 @@ class TestPassage < ApplicationRecord
     test.questions.order(:id).where('id > ?', current_question.id).first
   end
 
+  def before_validation_set_next_question
+    self.current_question = next_question
+  end
 end
