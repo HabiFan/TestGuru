857cdc335f70afd8295b215dd605d1fee860cdc3
Fanis <habfanis@mail.ru>
2021-06-16 10:19:05 +0300

    Fixed task_9

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,7 +2,6 @@ class TestsController < ApplicationController
 
   before_action :authenticate_user!
   before_action :set_test, only: %i[edit show update destroy start]
-  before_action :set_user, only: :start
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
@@ -41,7 +40,7 @@ class TestsController < ApplicationController
   end
 
   def start
-    @user.tests.push(@test)
+    @current_user.tests.push(@test)
     redirect_to @user.test_passage(@test)
   end
 
@@ -49,10 +48,6 @@ class TestsController < ApplicationController
 
   def set_test
     @test = Test.find(params[:id])
-  end
-
-  def set_user
-    @user = @current_user
   end
 
   def test_params
