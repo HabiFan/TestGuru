aff88b6f8356eadb5a7fbe46db70ac4e7eeb6a71
Fanis <habfanis@mail.ru>
2021-06-21 10:18:10 +0300

    Fixed task_10

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,7 +1,6 @@
 class TestsController < ApplicationController
 
   before_action :authenticate_user!
-  before_action :set_test, only: %i[start]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
 
@@ -10,15 +9,12 @@ class TestsController < ApplicationController
   end
  
   def start
+    @test = Test.find(params[:id])
     current_user.tests.push(@test)
     redirect_to current_user.test_passage(@test)
   end
 
   private
-
-  def set_test
-    @test = Test.find(params[:id])
-  end
 
   def rescue_with_test_not_found
     render plain: 'Тест не найден'
