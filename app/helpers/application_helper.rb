da399f58a69e2f982748b1980240ba06a965f725
Фанис Хабибуллин <userfan@MacBook-Air-Fanis.local>
2021-07-10 09:56:24 +0300

    task_12

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 module ApplicationHelper
+  
+  ALERTS = { alert: 'danger', notice: 'primary' }.freeze
+
   def github_url(author, repo)
     link_to "Author: #{author}, repositories: #{repo}",
             "https://github.com/#{author}/#{repo}",
@@ -8,4 +11,9 @@ module ApplicationHelper
   def current_date_year
     Date.current.strftime("%Y")
   end
+
+  def alert_manager(key)
+    ALERTS[key.to_sym] || key
+  end
+
 end
