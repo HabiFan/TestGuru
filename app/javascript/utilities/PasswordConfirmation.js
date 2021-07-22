f054dcd8e81f0c1f5ea388e0b129e0e758dadd99
Фанис Хабибуллин <userfan@MacBook-Air-Fanis.local>
2021-07-22 21:12:36 +0300

    Fixed

--- app/javascript/utilities/PasswordConfirmation.js
+++ app/javascript/utilities/PasswordConfirmation.js
@@ -4,13 +4,12 @@ export class PasswordConfirmation {
     this.password = form.elements.password
     this.confirm_password = form.elements.confirm_password
 
-    this.setup()
-  }
-
-  setup() {
     this.form.addEventListener('input', event => {
-      if (this.confirm_password.value != '') this.checkPasswords()
-      else this.resetDefaultStyle()
+      if (this.confirm_password.value != '') {
+        this.checkPasswords()
+      } else { 
+        this.resetDefaultStyle()
+      }
     })
   }
 
@@ -32,5 +31,4 @@ export class PasswordConfirmation {
     this.password.style.borderColor = '#ced4da'
     this.confirm_password.style.borderColor = '#ced4da'
   }
-
 }
--- app/javascript/utilities/SortedTable.js
+++ app/javascript/utilities/SortedTable.js
@@ -1,54 +1,23 @@
+
 export class SortedTable {
-  constructor(table_id) {
-    this.table = document.getElementById(table_id)
-    this.selectedNode = this.table.querySelector(".sort-by-title")
-    this.rows = this.table.querySelectorAll('tr')
-    this.sortedRows = []
-    this.sortedTable = document.createElement('table')
-    this.tblBody = document.createElement("tbody");
-    this.arrowUp = this.selectedNode.querySelector('.octicon-arrow-up')
-    this.arrowDown = this.selectedNode.querySelector('.octicon-arrow-down')
-
-    this.setup()
+  constructor(table) {
+    table.addEventListener('click', this.sortRowsByTitle)
   }
 
-  setup() {
-    const control = document.querySelector(".sort-by-title")
+  sortRowsByTitle() {
+    const table = document.querySelector('table')
+    const sortedRows = Array.from(table.rows).slice(1)
 
-    if (control) control.addEventListener('click', event => {
-      this.sortRowsByColumnName()
-    })
-  }
-
-  sortRowsByColumnName() {
-    const ColumnPos = this.selectedNode.cellIndex
-
-    for (var i = 1; i < this.rows.length; i++) {
-      this.sortedRows.push(this.rows[i])
+    if (this.querySelector('.octicon-arrow-up').classList.contains('hide')) {
+      sortedRows.sort((rowA, rowB) => rowA.cells[0].innerHTML > rowB.cells[0].innerHTML ? 1 : -1)
+      this.querySelector('.octicon-arrow-up').classList.remove('hide')
+      this.querySelector('.octicon-arrow-down').classList.add('hide')
+    } 
+    else {
+      sortedRows.sort((rowA, rowB) => rowA.cells[0].innerHTML > rowB.cells[0].innerHTML ? -1 : 1)
+      this.querySelector('.octicon-arrow-up').classList.add('hide')
+      this.querySelector('.octicon-arrow-down').classList.remove('hide')
     }
-
-    if (this.arrowUp.classList.contains('hide')) {
-      this.sortedRows.sort((rowA, rowB) => rowA.cells[ColumnPos].innerHTML > rowB.cells[ColumnPos].innerHTML ? 1 : -1)
-      this.arrowUp.classList.remove('hide')
-      this.arrowDown.classList.add('hide')
-    } else {
-      this.sortedRows.sort((rowA, rowB) => rowA.cells[ColumnPos].innerHTML > rowB.cells[ColumnPos].innerHTML ? -1 : 1)
-      this.arrowDown.classList.remove('hide')
-      this.arrowUp.classList.add('hide')
-    }
-
-    this.createSortedTable()
-  }
-
-  createSortedTable() {
-    this.sortedTable.classList.add('table', 'table-bordered', 'mt-2')
-    this.tblBody.appendChild(this.rows[0])
-
-    for (var i = 0; i < this.sortedRows.length; i++) {
-      this.tblBody.appendChild(this.sortedRows[i])
-    }
-    
-    this.sortedTable.appendChild(this.tblBody)
-    if (this.table.parentNode) this.table.parentNode.replaceChild(this.sortedTable, this.table)
+    table.tBodies[0].append(...sortedRows)
   }
 }
